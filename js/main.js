e470b97c27c239873dba3f4c8f7f0e90f7ac796a
Alexandre Andrieux <alexandre.andrieux@simplx.fr>
2018-02-07 14:02:27 +0100

    Base mechanics for handling IE with User-Agent sniffing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+	
+	// If IE, display fallback view and return false
+	if (isIE()) {
+		var ieBlock = document.getElementById('ie-fallback');
+		document.getElementsByTagName('body')[0].innerHTML = ieBlock.outerHTML;
+		ieBlock.style.display = 'inherit';
+		return false;
+	}
+	
     // App views
     var HomeComponent = {
         template: jQuery('#home-template').html(),
@@ -283,4 +291,19 @@ function randomizePaths(paths, maxNumber) {
     }
 
     return output;
+}
+
+function isIE() {
+	var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    return (msie > 0 || !!ua.match(/Trident.*rv\:11\./));
+}
+
+function isIEorEdge() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+	var edge = ua.indexOf('Edge/');
+
+    return (msie > 0 || !!ua.match(/Trident.*rv\:11\./) || edge > 0); 
 }
