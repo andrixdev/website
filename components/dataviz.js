67f00b08f3bb7978124d46502abfbc06478abcc4
Alexandre Andrieux <alex@icosacid.com>
2021-02-04 13:02:46 +0100

    Mechanics for gradient of shame in web projects

--- components/dataviz.js
+++ components/dataviz.js
@@ -61,6 +61,21 @@ Projects.DOMlisteners = function() {
 			if (!!goToLink) window.open(goToLink, '_blank');
 		}
 	});
+
+	// Show mores
+	jQuery('.web-dataviz-projects .student-misc-projects').hide();
+	jQuery('.web-dataviz-projects .showmore-toggle-bar.load-hum-projects').hide();
+	jQuery('.web-dataviz-projects .hum-projects').hide();
+
+	jQuery('.web-dataviz-projects .showmore-toggle-bar.load-student-misc-projects').on('click', function () {
+		jQuery('.web-dataviz-projects .student-misc-projects').fadeIn(500);
+		jQuery(this).remove();
+		jQuery('.web-dataviz-projects .showmore-toggle-bar.load-hum-projects').show();
+	});
+	jQuery('.web-dataviz-projects .showmore-toggle-bar.load-hum-projects').on('click', function () {
+		jQuery('.web-dataviz-projects .hum-projects').fadeIn(500);
+		jQuery(this).remove();
+	});
 			
 	// Header radios
 	/*
