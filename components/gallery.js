6bb57c70cf974de4ba0e10cead5660342c4b7953
Alex Andrix <alex@alexandrix.com>
2023-01-31 19:01:07 +0100

    Uprooting more jQuery and revising Gallery init/update sequence

--- components/gallery.js
+++ components/gallery.js
@@ -1,4 +1,5 @@
 let Gallery = {
+	isInit: false,
 	artworksData: [],
 	mouseIsOverFront: false,
 	isFrontShown: false,
@@ -6,13 +7,7 @@ let Gallery = {
 	timeout: undefined,
 	currentImageObject: undefined
 }
-
-/**
- * Fills the DOM with artworks.xml
- * @param {Function} extraCallback Function called after the DOM is filled
- */
 Gallery.loadArtworks = (extraCallback) => {
-
 	let handleXML = (xml) => {
 		Gallery.artworksData = []
 		xml.querySelectorAll("artwork").forEach((el) => {
@@ -41,14 +36,13 @@ Gallery.loadArtworks = (extraCallback) => {
 		})
 		
 	}
-
 	const xhr = new XMLHttpRequest()
-
 	xhr.open("GET", "data/artworks.xml", true)
 	xhr.onload = (e) => {
 		if (xhr.readyState === 4) {
 			if (xhr.status === 200) {
 				handleXML(xhr.responseXML)
+				extraCallback()
 			} else {
 				console.error("Failed retrieving artworks data.")
 				console.error(xhr.statusText)
@@ -59,40 +53,27 @@ Gallery.loadArtworks = (extraCallback) => {
 		console.error(xhr.statusText)
 	}
 	xhr.send(null)
-
 }
-
-/**
- * Assuming the global array artworksData is filled
- * @param {Number} id ID of artwork
- * @returns {Object} Artwork object
- */
 Gallery.getArtwork = (id) => {
+	console.log("Getting artwork with id " + id)
 	return Gallery.artworksData.filter((artwork) => {
-		"art-id-" + artwork.id == id 
+		return "art-id-" + artwork.id == id
 	})[0]
 }
-
-/**
- * Listeners on each artwork
- */
 Gallery.initListeners = () => {
 	// Artworks blocks
 	document.querySelectorAll(".artwork").forEach((el) => {
-		el.removeEventListener('click')
-		el.addEventListener('click', () => {
+		let onclick = () => {
 			// Update route history
 			Global.router.push({
-				path: 'gallery', query: { artwork: jQuery(this).attr('id') }
+				path: 'gallery', query: { artwork: el.id }
 			})
-		})
+		}
+		el.removeEventListener('click', onclick) // Justin Case
+		el.addEventListener('click', onclick)
 	})
 }
-
-/**
- * Listeners for front view
- */
-Gallery.frontListeners = function () {
+Gallery.frontListeners = () => {
 	jQuery('#front .zoom').off().on({
 		mouseenter: function () {
             Gallery.mouseIsOverFront = true;
@@ -101,154 +82,117 @@ Gallery.frontListeners = function () {
             Gallery.mouseIsOverFront = false;
             Gallery.hideFrontDetails();
 		}
-	});
+	})
 	jQuery('#front').off().on({
 		mouseup: function () {
 			if (Gallery.isFrontShown && !Gallery.mouseIsOverFront) {
 				Global.router.push({
-					path: 'gallery', query: { artwork: 0 }
+					path: 'gallery'
 				});
 			}
 		}
-	});
+	})
 	jQuery('#front .zoom .quit').off().on({
 		mouseup: function () {
 			Global.router.push({
-				path: 'gallery', query: { artwork: 0 }
-			});
+				path: 'gallery'
+			})
 		}
-	});
-	let resizingEventCount = 0;
+	})
+	let resizingEventCount = 0
 	jQuery(window).off().on('resize', function () {
-		resizingEventCount++;
+		resizingEventCount++
 		setTimeout(function () {
-			resizingEventCount--;
+			resizingEventCount--
 			if (resizingEventCount == 0) {
 				// Actual resize action
-				Gallery.adjustFrontImageSize();
+				Gallery.adjustFrontImageSize()
 			}
-		}, 500);
+		}, 500)
 		// Ca marche du feu de Dieu, comme disait l'autre
-	});
-};
-
-/**
- * DOM filler called on demand (click on an artwork)
- */
+	})
+}
 Gallery.fillFront = (id) => {
-
-	let alg = Gallery.getArtwork(id)
-	if (!alg) return false
+	let artwork = Gallery.getArtwork(id)
+	if (!artwork) return false
 
 	Gallery.loadingOn()
 	// Clear previous img
-    jQuery('#front .zoom img.fullone').attr('src', '')
+	document.querySelector('#front .zoom img.fullone').setAttribute('src', "")
 
-	let src = alg.src
-	let title = alg.title
-	let description = alg.description
+	let src = artwork.src
+	let title = artwork.title
+	let description = artwork.description
 
 	// Image
 	let newImg = new Image()
 	Gallery.currentImageObject = newImg// Store globally for .on('resize') action without reloading image
-	newImg.onload = function () {
-		jQuery('#front .zoom img.fullone').attr('src', '')
+	newImg.onload = () => {
+		document.querySelector('#front .zoom img.fullone').setAttribute('src', "")
 
 		Gallery.adjustFrontImageSize()
 
 		// Fake timeout
 		clearTimeout(Gallery.timeout)
-		let self = this
-		Gallery.timeout = setTimeout(function () {
+		Gallery.timeout = setTimeout(() => {
             Gallery.loadingOff()
-			jQuery('#front .zoom img.fullone').attr('src', self.src)
+			document.querySelector('#front .zoom img.fullone').setAttribute('src', src)
 		}, 300)
 	};
 	newImg.src = src
 	newImg.alt = title
 
 	// Description
-	jQuery('#front .zoom .details .description p.title').html(title)
-	jQuery('#front .zoom .details .description p.text').html(description)
-
-	return true
+	document.querySelector('#front .zoom .details .description p.title').innerHTML = title
+	document.querySelector('#front .zoom .details .description p.text').innerHTML = description
 }
-
 /**
  * Computes preview width and height based on current window size
  * And applies CSS rules to both .zoom container and image
  *
  * @returns Object {w, h}
  */
-Gallery.adjustFrontImageSize = function () {
+Gallery.adjustFrontImageSize = () => {
 	let h = Gallery.currentImageObject.height;
 	let w = Gallery.currentImageObject.width;
 
 	// If image is bigger than 0.9*window, rescale
-	let ratioW = w / (0.9 * jQuery(window).width());
-	let ratioH = h / (0.9 * jQuery(window).height());
-	let ratio = Math.max(ratioH, ratioW);
+	let ratioW = w / (0.9 * jQuery(window).width())
+	let ratioH = h / (0.9 * jQuery(window).height())
+	let ratio = Math.max(ratioH, ratioW)
 	if (ratio > 1 && ratioW > ratioH) {
 		h /= ratioW;
 		w /= ratioW;
 	} else if (ratio > 1 && ratioH > ratioW) {
-		h /= ratioH;
-		w /= ratioH;
+		h /= ratioH
+		w /= ratioH
 	}
 
-	jQuery('#front .zoom, #front .zoom img.fullone').css('height', h).css('width', w);
-};
-
-/**
- * Shows front view
- */
-Gallery.showFront = function () {
+	jQuery('#front .zoom, #front .zoom img.fullone').css('height', h).css('width', w)
+}
+Gallery.showFront = () => {
 	Gallery.isFrontShown = true
 
-	jQuery('#front').css('display', 'inherit')
+	document.getElementById('front').classList.remove('hidden')
 }
-
-/**
- * Hides front view
- */
 Gallery.hideFront = function () {
 	Gallery.isFrontShown = false
 
-	jQuery('#front').css('display', 'none')
+	document.getElementById('front').classList.add('hidden')
 }
-
-/**
- * Shows details in front view
- */
-Gallery.showFrontDetails = function () {
-	jQuery('#front .zoom .details').css('opacity', 1)
+Gallery.showFrontDetails = () => {
+	document.querySelector('#front .zoom .details').classList.add('visible')
 }
-
-/**
- * Hides details in front view
- */
-Gallery.hideFrontDetails = function () {
-	jQuery('#front .zoom .details').css('opacity', 0)
-};
-
-/**
- * Loading icon within front view - Turn on
- */
+Gallery.hideFrontDetails = () => {
+	document.querySelector('#front .zoom .details').classList.remove('visible')
+}
 Gallery.loadingOn = function () {
-	jQuery('.zoom .loader').show()
+	document.querySelector('.zoom .loader').classList.remove('hidden')
 }
-
-/**
- * Loading icon within front view - Turn off
- */
 Gallery.loadingOff = function () {
-	jQuery('.zoom .loader').hide()
+	document.querySelector('.zoom .loader').classList.add('hidden')
 }
-
-/**
- * Initializer
- */
-Gallery.go = () => {
+Gallery.init = () => {
     Gallery.loadArtworks(() => {
         Gallery.initListeners()
         Gallery.frontListeners()
@@ -263,16 +207,20 @@ Gallery.go = () => {
 	    }
     })
 }
-Gallery.applyState = () => {
-	if (!Gallery.artworksData.length) Gallery.go()
-	else {
-		if (Gallery.shouldShowFront) {
-			// Get ID
-			let artworkID = location.search.split('artwork=')[1]
-			if (Gallery.fillFront(artworkID)) Gallery.showFront()
-		} else {
-			// Close potentially open front mode
-			Gallery.hideFront()
-		}
+Gallery.update = () => {
+	// Init if not done yet
+	if (!Gallery.isInit) {
+		Gallery.init()
+		Gallery.isInit = true
+	}
+	// Gallery.shouldShowFront might have been modified by router
+	if (Gallery.shouldShowFront && !Gallery.isFrontShown) {
+		// Get ID
+		let artworkID = location.search.split('artwork=')[1]
+		Gallery.fillFront(artworkID)
+		Gallery.showFront()
+	} else {
+		// Close potentially open front mode
+		Gallery.hideFront()
 	}
 }
--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // App components (short ones, the others are in separate files)
-    var GalleryComponent = {
+    let GalleryComponent = {
         template: jQuery('#gallery-template').html(),
         data: function () {
             return { }
         },
-        mounted: function ()  { Gallery.go(); }
-    };
-    var FilmsComponent = {
+        mounted: function ()  {
+            // This effing method is called every time, same for other Vue hooks, I'm going full vanillaJS very soon.
+            Gallery.update()
+        }
+    }
+    let FilmsComponent = {
         template: jQuery('#films-template').html(),
-        data: function () {
+        data: () => {
             return { }
         },
-        mounted: function () { }
-    };
-    var AndrixBrandComponent = {
+        mounted: () => { }
+    }
+    let AndrixBrandComponent = {
         template: jQuery('#andrix-brand-template').html(),
-        data: function () {
+        data: () => {
             return { }
         },
-        mounted: function () { }
-    };
-    var CVComponent = {
+        mounted: () => { }
+    }
+    let CVComponent = {
         template: jQuery('#cv-template').html(),
-        data: function () {
+        data: () => {
             return { }
         },
-        mounted: function () { CV.go(); }
-    };
+        mounted: () => { CV.go() }
+    }
 
     // Custom components
-    Vue.component('vue-title', HeadTitleComponent);
+    Vue.component('vue-title', HeadTitleComponent)
     Vue.component('geom', {
         template: jQuery('#geom-template').html(),
         props: ['baseSides', 'header', 'rotate'],
@@ -41,44 +44,44 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         },
         mounted: function () {
-            this.newSides = this.baseSides;
-            var self = this;
-            var glitch = function () {
-                self.newSides = self.newSides == 4 ? 0 : 4;
+            this.newSides = this.baseSides
+            let self = this
+            let glitch = () => {
+                self.newSides = self.newSides == 4 ? 0 : 4
 
-                setTimeout(function () {
-                    self.newSides = self.newSides == 4 ? 0 : 4;
-                }, 40);
-            };
+                setTimeout(() => {
+                    self.newSides = self.newSides == 4 ? 0 : 4
+                }, 40)
+            }
 
             // Glitch loop
-            setInterval(function () {
+            setInterval(() => {
                 // 10% chance of glitch
                 if (Math.random() < 0.1) {
                     // Glitch up to 3 times
-                    var glitches = Math.ceil(3 * Math.random());
-                    for (var g = 0; g < glitches; g++) {
-                        setTimeout(function () {
-                            glitch();
-                        }, g * 80);
+                    let glitches = Math.ceil(3 * Math.random())
+                    for (let g = 0; g < glitches; g++) {
+                        setTimeout(() => {
+                            glitch()
+                        }, g * 80)
                     }
                 }
-            }, 1000);
+            }, 1000)
 			
         }
-    });
+    })
     Vue.component('logo-links', {
         template: jQuery('#logo-links-template').html()
-    });
+    })
     Vue.component('coming-soon', {
         template: jQuery('#coming-soon-template').html()
-    });
+    })
     Vue.component('inline-svg-logo', {
         template: jQuery('#inline-svg-logo-template').html()
-    });
+    })
 
     // App routes
-    var routes = [
+    let routes = [
         { path: '/', component: HomeComponent },
         { path: '/gallery', component: GalleryComponent },
         { path: '/films', component: FilmsComponent },
@@ -87,42 +90,40 @@ document.addEventListener('DOMContentLoaded', () => {
         { path: '/contact', component: ContactComponent },
         { path: '/andrix', component: AndrixBrandComponent },
         { path: '/cv', component: CVComponent },
-        { path: '/projects', redirect: '/web-dataviz' },
-        { path: '/web-dataviz', redirect: '/more' },
+        { path: '/projects', redirect: '/web-dataviz' }, { path: '/web-dataviz', redirect: '/more' },
         { path: '/creative-coding', redirect: '/more' },
         { path: '*', component: NotFoundComponent }
-    ];
+    ]
 
     // Build router
     Global.router = new VueRouter({
         routes: routes,
         mode: 'history'
-    });
+    })
 
     // Scroll to top on route change
-    Global.router.beforeEach(function (to, from, next) {
+    Global.router.beforeEach((to, from, next) => {
 
-        setTimeout(function () {
+        setTimeout(() => {
         	// Scroll back to top unless it's some gallery internal navigation
 	        if (!(to.fullPath.indexOf('gallery') > -1 && from.fullPath.indexOf('gallery') > -1)) {
-		        window.scrollTo(0, 0);
+		        window.scrollTo(0, 0)
 	        }
-        }, 100);
-        next();
+        }, 100)
+        next()
 
 	    // If back button from front Gallery mode, close it
-	    console.log(to.fullPath, from.fullPath);
+	    console.log("Navigating from " + from.fullPath + " to " + to.fullPath)
 	    if (to.fullPath.indexOf('gallery') > -1 || from.fullPath.indexOf('gallery') > -1) {
-		    console.log(to.fullPath, from.fullPath);
 		    // Going to Gallery or coming from it
-		    if (to.fullPath.indexOf('art-id') === -1) Gallery.shouldShowFront = false;
-		    else Gallery.shouldShowFront = true;
-		    Gallery.applyState();
+		    if (to.fullPath.indexOf('art-id') === -1) Gallery.shouldShowFront = false
+		    else Gallery.shouldShowFront = true
+		    Gallery.update()
 	    }
 
-    });
+    })
 
-    var app = new Vue({
+    let app = new Vue({
         router: Global.router,
         el: '#alexandrix-website',
         data: {
@@ -131,13 +132,13 @@ document.addEventListener('DOMContentLoaded', () => {
             saturation: 100
         },
         methods: {
-            rotateLeHue: function () {
-                this.hueAngle = Math.round(360 * Math.random());
+            rotateLeHue: () => {
+                this.hueAngle = Math.round(360 * Math.random())
             }
         },
-		mounted: function () {
+		mounted: () => {
 
 		}
-    });
+    })
 
-});
+})
